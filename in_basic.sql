ce2ee2e6c35a7b7ad06bc59459d116b179cb7ede
moglee2699 <abishailearning@gmail.com>
2025-04-12 14:41:05 +0200

    Update in_basic.sql

--- in_basic.sql
+++ in_basic.sql
@@ -2,4 +2,8 @@ SELECT DISTINCT customer_id FROM orders
 WHERE product_name IN ('Laptop','Tablet','Smartphone');
 
 SELECT * FROM customer
-WHERE customer_id IN (123,212,323,243,353,432)
+WHERE customer_id IN (123,212,323,243,353,432);
+
+SELECT * FROM payment
+WHERE customer_id IN (12,25,67,93,124,234)
+AND amount IN (4.99,7.99,9.99) AND payment_date BETWEEN '2020-01-01' AND '2020-01-31 23:59';
